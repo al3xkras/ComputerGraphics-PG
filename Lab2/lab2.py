0e0ebcb56674d29ad864a4c38776b8d48840556f
al3xkras <alexandr.krasovskiy@gmail.com>
2022-10-19 11:50:53 +0200

    Lab2: Manipulator: add angle restrictions

--- Lab2/lab2.py
+++ Lab2/lab2.py
@@ -3,15 +3,16 @@ from math import sin, cos
 
 import pygame.gfxdraw
 
+
 class Manipulator:
     _hand_prefix = "/hand.png"
     _wrist_prefix = "/wrist.png"
     graphics_path = "./graphics/"
-    arm_width = 7
+    arm_width = 5
     hand_width = 20
     hand_ratio = 1.6
 
-    def __init__(self, M, w3, w2, w1):
+    def __init__(self, M, w3, w2, w1, params=None):
         self.hand = None
         self.arm2 = None
         self.arm1 = None
@@ -20,6 +21,16 @@ class Manipulator:
             Manipulator.distance(self.vertices[i], self.vertices[i + 1]) for i in range(len(self.vertices) - 1))
         self._scale = [1, 1, 1]
         self._rotate = [0, 0, 0, 0]
+        if params is None:
+            params = [
+                tuple([(-180, 180)] + [(-120, 120) for _ in range(len(self._rotate) - 1)]),  # angle restrictions
+            ]
+        else:
+            assert len(params) == 1
+            assert len(params[0]) == len(self._rotate)
+
+        self.angle_limits = params[0]
+
         try:
             self.loadAssets()
         except:
@@ -56,11 +67,12 @@ class Manipulator:
         vertices = self._eval_vertices()
         pygame.draw.circle(surface, "black", vertices[0], Manipulator.arm_width * 2, width=1)
         for _i in range(1, len(vertices)):
-            self._draw_arm(surface, vertices[_i - 1], vertices[_i], Manipulator.arm_width, self.arm1)
+            Manipulator._draw_arm(surface, vertices[_i - 1], vertices[_i], Manipulator.arm_width, self.arm1)
 
         v1 = vertices[3]
         sc = [vertices[2][0] - v1[0], vertices[2][1] - v1[1]]
-        sc = [-x * Manipulator.hand_ratio for x in sc]
+        l = Manipulator.distance((0, 0), sc)
+        sc = [-x / l * Manipulator.hand_width * Manipulator.hand_ratio for x in sc]
         v2 = [v1[i] + sc[i] for i in range(len(v1))]
         v2 = Manipulator.rotate_around_point(v2, self._rotate[3] / 180 * math.pi, v1)
 
@@ -71,7 +83,11 @@ class Manipulator:
 
     def rotate(self, vert_num, angle):
         self._rotate[vert_num] += angle
-        self._rotate[vert_num] = self._rotate[vert_num] % 360
+        min_r, max_r = self.angle_limits[vert_num]
+        self._rotate[vert_num] = min(max_r, max(min_r, self._rotate[vert_num]))
+        if abs(self._rotate[vert_num] >= 180):
+            self._rotate[vert_num] = -self._rotate[vert_num]
+        print(self._rotate[vert_num])
 
     @staticmethod
     def rotate_surface(surface, angle, pivot, offset):
@@ -95,22 +111,23 @@ class Manipulator:
         V = [x for x in self.vertices]
         for k in range(len(self._rotate)):
             r = self._rotate[k]
-            for i in range(k + 1, len(V)):
-                V[i] = Manipulator.rotate_around_point(V[i], r / 180 * math.pi, V[k])
+            for _i in range(k + 1, len(V)):
+                V[_i] = Manipulator.rotate_around_point(V[_i], r / 180 * math.pi, V[k])
         return V
 
     def _eval_scaled_points(self, V):
-        for i in range(1, len(V)):
-            vec_x = V[i][0] - V[i - 1][0]
-            vec_y = V[i][1] - V[i - 1][1]
-            vec_x = vec_x * self._scale[i - 1] - vec_x
-            vec_y = vec_y * self._scale[i - 1] - vec_y
-            for j in range(i, len(V)):
+        for _i in range(1, len(V)):
+            vec_x = V[_i][0] - V[_i - 1][0]
+            vec_y = V[_i][1] - V[_i - 1][1]
+            vec_x = vec_x * self._scale[_i - 1] - vec_x
+            vec_y = vec_y * self._scale[_i - 1] - vec_y
+            for j in range(_i, len(V)):
                 V[j][0] += vec_x
                 V[j][1] += vec_y
         return V
 
-    def _draw_arm(self, surface: pygame.Surface, V1, V2, arm_width, arm=None, ):
+    @staticmethod
+    def _draw_arm(surface: pygame.Surface, V1, V2, arm_width, arm=None, ):
         if arm is None:
             pygame.draw.line(surface, "black", V1, V2)
         else:
@@ -172,14 +189,12 @@ if __name__ == '__main__':
         # Usage example
         s = pygame.Surface(surf_size)
         s.fill((0xff, 0xff, 0xff))
-        m.rotate(0, 1)
-        m.rotate(1, 2)
+        m.rotate(0, 10)
+        m.rotate(1, -2)
         m.rotate(2, 3)
-        m.rotate(3, 5)
+        m.rotate(3, -5)
         i += 1
         i = i % 200
-        m.scale(0, 1.6)
-        m.scale(1, 1 + i / 100)
         m.draw(s)
         screen.blit(pygame.transform.scale(s, WINDOW_SIZE), (0, 0))
 
