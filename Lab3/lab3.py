e71e323d110620c57e5cbb2a83f1db059370cf65
al3xkras <alexandr.krasovskiy@gmail.com>
2022-11-13 14:08:43 +0100

    Lab3: implement spacecraft; bind hotkeys (l/r, u/d), add contestant spacecraft

--- Lab3/lab3.py
+++ Lab3/lab3.py
@@ -8,27 +8,45 @@ from background import FalseDepthBackground
 from spacecraft import Spacecraft
 
 
+class SpaceshipInfo:
+    def __init__(self, warp_getter):
+        self.warp_getter = warp_getter
+
+    def draw(self, screen: pygame.Surface):
+        my_font = pygame.font.SysFont('Comic Sans MS', 30)
+        text = my_font.render("warp: %d" % self.warp_getter(), False, (255, 255, 255))
+        screen.blit(text, (15, 15))
+
+
 class Lab3:
-    WINDOW_SIZE = (700, 700)
+    WINDOW_SIZE = (900, 700)
     WINDOW_POSITION = (500, 30)
     FONT_SIZE = 24
     FONT_BOLD = False
     FLAGS = pygame.DOUBLEBUF | pygame.HWSURFACE
-
+    warp_min = 0
+    warp_max = 10
+    contestant_spaceship_warp_speed = 5
     def __init__(self, initial_speed=0, bg_layer_count=15, bg_depth=200):
         self.screen = None
         self.background = None
         self.spacecraft = None
+        self.contestant_spacecraft = None
         self.hotkey_thread = None
-        self.initial_speed=initial_speed
+        self.spaceship_info = None
+        self.initial_speed = initial_speed
         self.bg_layer_count = bg_layer_count
         self.bg_depth = bg_depth
         self.warp = 0
+        self.distance_traveled = 0
+        self.distance_traveled_spaceship1 = 0
+        self.distance_traveled_spaceship2 = 0
 
     def setup(self, setup_hotkeys=True):
         self.background = FalseDepthBackground(self.bg_layer_count, self.initial_speed, self.bg_depth)
-        self.spacecraft = Spacecraft()
-
+        self.spacecraft = Spacecraft(displaySize=Lab3.WINDOW_SIZE)
+        self.spaceship_info = SpaceshipInfo(lambda: self.warp)
+        self.contestant_spacecraft = Spacecraft(displaySize=Lab3.WINDOW_SIZE,sprite="contestant_spaceship.png")
         if setup_hotkeys:
             self.hotkey_thread = Thread(target=self._setup_hotkeys)
             self.hotkey_thread.daemon = True
@@ -37,19 +55,39 @@ class Lab3:
         self.screen = self._pygame_screen_setup()
 
     def speed_up(self):
-        self.background.speed_up(100)
+        if self.warp >= Lab3.warp_max:
+            return
+        self.warp += 1
+        self.background.speed_up(50)
         print("speed up")
 
     def slow_down(self):
-        self.background.slow_down(100)
+        if self.warp <= Lab3.warp_min:
+            return
+        self.warp -= 1
+        self.background.slow_down(50)
         print("slow down")
 
     def _setup_hotkeys(self):
-        with keyboard.GlobalHotKeys({
-            'a': self.speed_up,
-            'b': self.slow_down,
-            '<ctrl>+c': quit}) as h:
-            h.join()
+        def on_press(key):
+            if hasattr(key, 'vk') and key.vk == 12:
+                self.stop()
+
+            if key == keyboard.Key.esc:
+                return False  # stop listener
+
+            if hasattr(key, 'name'):
+                if key.name == "left":
+                    self.slow_down()
+                elif key.name == "right":
+                    self.speed_up()
+                elif key.name == "up":
+                    self.spacecraft.move(-Lab3.WINDOW_SIZE[1] / 10)
+                elif key.name == "down":
+                    self.spacecraft.move(Lab3.WINDOW_SIZE[1] / 10)
+
+        with keyboard.Listener(on_press=on_press) as l:
+            l.join()
 
     def _pygame_screen_setup(self):
         pygame.init()
@@ -61,9 +99,8 @@ class Lab3:
         exit()
 
     def mainloop(self):
-        assert self.background is not None
-        assert self.spacecraft is not None
 
+        self.spacecraft.move(Lab3.WINDOW_SIZE[1] / 2)
         clock = pygame.time.Clock()
         while True:
             clock.tick(30)
@@ -75,16 +112,25 @@ class Lab3:
             screen.fill((0x00, 0x00, 0x00))
 
             self.background.draw(screen)
-            # self.spacecraft.draw(screen)
+            self.spaceship_info.draw(screen)
 
+            self.distance_traveled += self.warp*2
+            self.distance_traveled_spaceship1 += self.warp*2
+            self.distance_traveled_spaceship2 += Lab3.contestant_spaceship_warp_speed*2
+
+            self.spacecraft.speed_up(self.distance_traveled_spaceship1-self.distance_traveled)
+            self.contestant_spacecraft.speed_up(self.distance_traveled_spaceship2-self.distance_traveled)
+
+            self.contestant_spacecraft.draw(screen)
+            self.spacecraft.draw(screen)
             pygame.display.flip()
 
 
 if __name__ == '__main__':
     from pygame.locals import *
 
-    lab3 = Lab3(bg_layer_count=15, initial_speed=0,bg_depth=200)
+    lab3 = Lab3(bg_layer_count=15, initial_speed=0, bg_depth=200)
     lab3.setup()
     lab3.background.reverse()
-    lab3.background.star_size=7
+    lab3.background.star_size = 7
     lab3.mainloop()
--- Lab3/spacecraft.py
+++ Lab3/spacecraft.py
@@ -2,18 +2,47 @@ import pygame.gfxdraw
 
 
 class Spacecraft:
-    def __init__(self):
-        pass
+    graphics_path = "./graphics/"
+    spaceship_size = 150
+    bgspeed = 10
+    initialSpeed = 5
 
-    def draw(self, surface: pygame.Surface):
-        pygame.draw.line(surface, "black", (0, 0), (100, 200))
-        pass
+    def __init__(self, displaySize, x=100, y=100, sprite="spaceship.png"):
+        self.speed = 0
+        self.displaySize = displaySize
+        self.sprite = sprite
+        self.y = y
+        self.x = x
+        self.vertices2 = (x, y)
+        self.spaceship = None
+        self._load_spaceship()
 
-    def move(self, pixels):
-        pass
+    def _load_spaceship(self):
+        self.spaceship = pygame.image.load(Spacecraft.graphics_path + self.sprite)
+        self.spaceship = pygame.transform.scale(self.spaceship, (
+            int(self.spaceship_size * self.spaceship.get_width() / self.spaceship.get_height()), self.spaceship_size))
+
+    def draw(self, layer: pygame.Surface):
+        layer.blit(self.spaceship, (self.x+self.speed, self.y))
+
+    def draw2(self, layer: pygame.Surface):
+        layer.blit(self.spaceship, self.vertices2)
+
+    def move(self, delta):
+        clock = pygame.time.Clock()
+        execTime = 500
+        delay = 60
+        n = execTime // delay
+        y_initial = self.y
+        for i in range(n + 1):
+            clock.tick(delay)
+            self.y = min(self.displaySize[1] - self.spaceship_size, max(0, y_initial + delta * i / n))
 
     def speed_up(self, delta):
-        pass
+        self.speed = delta
+
+    def slow_down(self, delta):
+        self.speed = delta
 
     def stop(self):
-        pass
+        self.speed = 0
